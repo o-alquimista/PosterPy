4368b426177d81e4eb0f900a73ab15cfb4a56273
Douglas Silva <doug.hs@protonmail.ch>
2019-09-03 18:27:25 -0300

    It is now capable of authenticating as a user

--- poster.py
+++ poster.py
@@ -1,13 +1,54 @@
+'''
+PosterPy, a fixture creator for web applications.
+
+Copyright 2019 Douglas Silva (0x9fd287d56ec107ac)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
 import random, string, requests
 from bs4 import BeautifulSoup
 
 # The URL of the request
-url = 'http://devdungeon.org/admin/new'
+url = 'http://devdungeon.org/en/admin/new'
+
+# The login URL
+loginURL = 'http://devdungeon.org/en/login'
+
+# Starts a session
+client = requests.Session()
+
+# Parse the response from the URL
+soup = BeautifulSoup(client.get(loginURL).text, 'html.parser')
+
+# Retrieve the CSRF token
+hidden_input = soup.find(id="login__token")
+csrf_token = hidden_input['value']
+
+# The login credentials
+payload = {
+  'username':'nanodano',
+  'password':'test',
+  '_csrf_token':csrf_token,
+}
+
+# Authenticate
+client.post(url=loginURL, data=payload)
+
+print('Authenticating...')
 
 for i in range(1, 101):
-    # Starts a session
-    client = requests.Session()
-
     # Parse the response from the URL
     soup = BeautifulSoup(client.get(url).text, 'html.parser')
 
@@ -33,3 +74,5 @@ for i in range(1, 101):
     client.post(url=url, data=payload)
 
     print('Item '+str(i)+' published.')
+
+print('All items published.')
